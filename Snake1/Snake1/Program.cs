fcf081bef86a0e5f14d9117495f50e85865bdf63
Myonass <tolmachev_maksim96@mail.ru>
2021-02-19 14:33:42 +0300

    Vertical & Horizontal line in Snake game

--- Snake1/Snake1/Program.cs
+++ Snake1/Snake1/Program.cs
@@ -13,25 +13,11 @@ namespace Snake1
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine line = new HorizontalLine(0, 40, 20, '+');
+            line.Draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2]; 
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            VerticalLine line1 = new VerticalLine(0, 20, 40, '-');
+            line1.Draw();
 
             Console.ReadLine();
         }
---
+++ Snake1/Snake1/VerticalLine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake1
+{
+
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yTop, int yBottom, int x, char symb)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, symb);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+
+}
+
+
+    
+
+
